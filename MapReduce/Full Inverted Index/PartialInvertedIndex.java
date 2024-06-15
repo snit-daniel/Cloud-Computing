a9fb3ddcfd9b2f23e5f4a748dd91237b3acd5c12
snit-daniel <101430904+snit-daniel@users.noreply.github.com>
2024-06-15 23:39:47 +0400

    Update PartialInvertedIndex.java

--- MapReduce/Full Inverted Index/PartialInvertedIndex.java
+++ MapReduce/Full Inverted Index/PartialInvertedIndex.java
@@ -1,39 +1,70 @@
 package org.myorg;
+
 import java.io.IOException;
 import java.util.*;
+
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.conf.*;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapred.*;
 import org.apache.hadoop.util.*;
+
 public class PartialInvertedIndex {
-public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
-private Text word = new Text();
-private IntWritable fileIndex = new IntWritable();
-public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
-FileSplit fileSplit = (FileSplit) reporter.getInputSplit();
-String fileName = fileSplit.getPath().getName();
-int fileNumber = Integer.parseInt(fileName.replaceAll("[^0-9]", ""));
-fileIndex.set(fileNumber);
-String line = value.toString();
-StringTokenizer tokenizer = new StringTokenizer(line);
-while (tokenizer.hasMoreTokens()) {
-word.set(tokenizer.nextToken());
-output.collect(word, fileIndex);
-}
-}
-}
-public static void main(String[] args) throws Exception {
-JobConf conf = new JobConf(PartialInvertedIndex.class);
-conf.setJobName("partialinvertedindex");
-conf.setOutputKeyClass(Text.class);
-conf.setOutputValueClass(IntWritable.class);
-conf.setMapperClass(Map.class);
-conf.setReducerClass(Reduce.class);
-conf.setInputFormat(TextInputFormat.class);
-conf.setOutputFormat(TextOutputFormat.class);
-FileInputFormat.setInputPaths(conf, new Path(args[0]));
-FileOutputFormat.setOutputPath(conf, new Path(args[1]));
-JobClient.runJob(conf);
-}
+
+    public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+        private Text word = new Text();
+        private IntWritable fileIndex = new IntWritable();
+
+        public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+            FileSplit fileSplit = (FileSplit) reporter.getInputSplit();
+            String fileName = fileSplit.getPath().getName();
+            int fileNumber = Integer.parseInt(fileName.replaceAll("[^0-9]", ""));
+            fileIndex.set(fileNumber);
+
+            String line = value.toString();
+            StringTokenizer tokenizer = new StringTokenizer(line);
+            while (tokenizer.hasMoreTokens()) {
+                word.set(tokenizer.nextToken());
+                output.collect(word, fileIndex);
+            }
+        }
+    }
+
+    public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, Text> {
+        public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
+            Set<Integer> fileIndices = new HashSet<>();
+            while (values.hasNext()) {
+                fileIndices.add(values.next().get());
+            }
+
+            StringBuilder result = new StringBuilder();
+            for (Integer index : fileIndices) {
+                if (result.length() > 0) {
+                    result.append(", ");
+                }
+                result.append(index);
+            }
+
+            output.collect(key, new Text("{" + result.toString() + "}"));
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        JobConf conf = new JobConf(PartialInvertedIndex.class);
+        conf.setJobName("partialinvertedindex");
+
+        conf.setOutputKeyClass(Text.class);
+        conf.setOutputValueClass(IntWritable.class);
+
+        conf.setMapperClass(Map.class);
+        conf.setReducerClass(Reduce.class);
+
+        conf.setInputFormat(TextInputFormat.class);
+        conf.setOutputFormat(TextOutputFormat.class);
+
+        FileInputFormat.setInputPaths(conf, new Path(args[0]));
+        FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+
+        JobClient.runJob(conf);
+    }
 }
